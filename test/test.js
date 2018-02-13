ad1654de216406c80db1ed358917547a0fea6862
Hector <hectoratiliotrujillo@gmail.com>
2018-02-12 20:24:31 -0500

    added test for #get

--- test/test.js
+++ test/test.js
@@ -9,8 +9,17 @@ describe('Thrower', function () {
         });
     });
     describe('#get()', function () {
-        it('should throw: Malformed error. No error was provided', function () {
+        it('should get: Malformed error. No error was provided', function () {
             expect(thrower.get()).to.equal('Malformed error. No error was provided')
+        });
+        it('should get: Malformed error. Please use an integer for an error code.', function () {
+            expect(thrower.get('1000')).to.equal('Malformed error. Please use an integer for an error code.')
+        });
+        it('should get: Malformed error. Code provided does not correspond to any error.', function () {
+            expect(thrower.get(900)).to.equal('Malformed error. Code provided does not correspond to any error.')
+        });
+        it('should get: A required field is missing.', function () {
+            expect(thrower.get(1100)).to.equal('A required field is missing.')
         });
     });
 });
